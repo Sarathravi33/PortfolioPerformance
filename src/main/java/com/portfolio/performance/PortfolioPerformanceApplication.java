62b916641e9d77b8fd9e5185c5d44d4e18787dac
sarathshadow@gmail.com <43153838+Sarathravi33@users.noreply.github.com>
2026-06-29 18:43:54 +0530

    Initial project setup

---
+++ src/main/java/com/portfolio/performance/PortfolioPerformanceApplication.java
@@ -0,0 +1,12 @@
+package com.portfolio.performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioPerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioPerformanceApplication.class, args);
+    }
+}
